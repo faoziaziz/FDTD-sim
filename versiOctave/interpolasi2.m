9dfc7e2ca30762e87680b2762328cf7c6cd1692a
Aziz Amerul Faozi <admin@labseni.com>
2019-02-22 16:53:08 +0700

    interpolasi2

---
+++ versiOctave/interpolasi2.m
@@ -0,0 +1,14 @@
+
+xa1 = linspace(-1,1,21);
+xa2 = linspace(-1,1,250);
+
+% Function
+f1 = exp(-xa1.^2/0.2^2);
+
+%Interpolate
+f2 = interp1(xa1, f1, xa2, 'cubic');
+
+plot(xa2, f2, '.b');
+hold on;
+plot(xa1, f1, 'o-r');
+hold off;
